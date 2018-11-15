a8e4feb3a7abb59e2a742643dd06392b9df5d53a
HanselLi <SuperManOfAndroid@163.com>
2018-11-15 19:44:48 +0800

    bugfix

--- app/src/main/java/sogou/mobile/custombar/MainActivity.java
+++ app/src/main/java/sogou/mobile/custombar/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView showTvContent;
+    private TextView textTip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         showTvContent = (TextView) findViewById(R.id.showTvContent);
-        final TextView textTip = (TextView) findViewById(R.id.textTip);
+        textTip = (TextView) findViewById(R.id.textTip);
         HorizontalScrollBall scrollBall = (HorizontalScrollBall) findViewById(R.id.scrollBall);
         scrollBall.setOnTextSizeChangeListener(new HorizontalScrollBall.OnTextSizeChangeListener() {
             @Override
@@ -29,15 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("hansel","textSize is : " + textSize);
                 showTvContent.setTextSize(textSize);
                 SpUtil.getInstance().setCurrentTextSize(textSize);
-                if (textSize == HorizontalScrollBall.mWebTextSizeZoom50){
-                    textTip.setVisibility(View.VISIBLE);
-                    textTip.setText(R.string.text_small_tip);
-                }else if (textSize >= HorizontalScrollBall.mWebTextSizeZoom150){
-                    textTip.setVisibility(View.VISIBLE);
-                    textTip.setText(R.string.text_big_tip);
-                }else {
-                    textTip.setVisibility(View.INVISIBLE);
-                }
+                refreshTipText(textSize);
             }
         });
 
@@ -46,11 +39,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        refreshShowTvTextSize();
+        refreshTextState();
     }
 
-    private void refreshShowTvTextSize() {
-        Log.d("hansel"," get stored text size : " + SpUtil.getInstance().getCurrentTextSize());
-        showTvContent.setTextSize(SpUtil.getInstance().getCurrentTextSize());
+    private void refreshTextState() {
+        int currentTextSize = SpUtil.getInstance().getCurrentTextSize();
+        showTvContent.setTextSize(currentTextSize);
+        refreshTipText(currentTextSize);
     }
+
+    private void refreshTipText(int currentTextSize){
+        if (currentTextSize == HorizontalScrollBall.mWebTextSizeZoom50){
+            textTip.setVisibility(View.VISIBLE);
+            textTip.setText(R.string.text_small_tip);
+        }else if (currentTextSize >= HorizontalScrollBall.mWebTextSizeZoom150){
+            textTip.setVisibility(View.VISIBLE);
+            textTip.setText(R.string.text_big_tip);
+        }else {
+            textTip.setVisibility(View.INVISIBLE);
+        }
+    }
+
 }
